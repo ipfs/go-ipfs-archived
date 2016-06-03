0698dd916ce7685f806be1d76ea7618182a56dfe
Jeromy <why@ipfs.io>
2016-06-02 19:41:19 -0700

    fix goroutine leak in routing resolve
    License: MIT
    Signed-off-by: Jeromy <why@ipfs.io>

--- namesys/routing.go
+++ namesys/routing.go
@@ -145,12 +145,14 @@ func (r *routingResolver) resolveOnce(ctx context.Context, name string) (path.Pa
 		if err != nil {
 			log.Warning("RoutingResolve get failed.")
 			resp <- err
+			return
 		}
 
 		entry = new(pb.IpnsEntry)
 		err = proto.Unmarshal(val, entry)
 		if err != nil {
 			resp <- err
+			return
 		}
 		resp <- nil
 	}()
@@ -160,6 +162,7 @@ func (r *routingResolver) resolveOnce(ctx context.Context, name string) (path.Pa
 		pubk, err := routing.GetPublicKey(r.routing, ctx, hash)
 		if err != nil {
 			resp <- err
+			return
 		}
 		pubkey = pubk
 		resp <- nil
