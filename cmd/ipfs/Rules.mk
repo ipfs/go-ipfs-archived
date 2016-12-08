267af282a7a8ccee0dcd233f49e68abc7a014c87
Jakub Sztandera <kubuxu@protonmail.ch>
2016-12-08 23:03:35 +0100

    Cleanup
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- cmd/ipfs/Rules.mk
+++ cmd/ipfs/Rules.mk
@@ -1,16 +1,14 @@
-p 				:= $(sp).x
-dirstack_$(sp)	:= $(d)
-d				:= $(dir)# keep track of dirs
-
+p := $(sp).x
+dirstack_$(sp) := $(d)# keep track of dirs
+d := $(dir)
 
 TGTS_$(d) := $(call go-curr-pkg-tgt)
 
 TGT_BIN += $(TGTS_$(d))
 CLEAN += $(TGTS_$(d))
 
-
 $(TGTS_$(d)): $(d) ALWAYS
 	$(go-build)
 
-d		:= $(dirstack_$(sp))
-sp		:= $(basename $(sp))
+d := $(dirstack_$(sp))
+sp := $(basename $(sp))
