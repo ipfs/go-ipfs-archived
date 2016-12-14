707097cf74691624e30b2a4ea63023070ab45eed
Jakub Sztandera <kubuxu@protonmail.ch>
2016-12-14 09:06:01 +0100

    Add pb files as order only dependencies
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- cmd/ipfs/Rules.mk
+++ cmd/ipfs/Rules.mk
@@ -6,7 +6,13 @@ CLEAN += $(TGTS_$(d))
 
 PATH := $(realpath $(d)):$(PATH)
 
-$(TGTS_$(d)): $(d) $$(DEPS_GO) ALWAYS # uses second expansion to collect all $(DEPS_GO)
+# depend on *.pb.go files in the repo as Order Only (as they shouldn't be rebuilt if exist)
+DPES_OO_$(d) := diagnostics/pb/diagnostics.pb.go exchange/bitswap/message/pb/message.pb.go
+DEPS_OO_$(d) += merkledag/pb/merkledag.pb.go namesys/pb/namesys.pb.go
+DEPS_OO_$(d) += pin/internal/pb/header.pb.go unixfs/pb/unixfs.pb.go
+
+# uses second expansion to collect all $(DEPS_GO)
+$(TGTS_$(d)): $(d) $$(DEPS_GO) ALWAYS | $(DPES_OO_$(d))
 	$(go-build)
 
 include mk/footer.mk
