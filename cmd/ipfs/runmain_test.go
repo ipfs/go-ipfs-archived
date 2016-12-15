1ee185de67ca3a9a61a9c41347ab8bef8b66a1cb
Jakub Sztandera <kubuxu@protonmail.ch>
2016-12-15 01:12:50 +0100

    cover: improve main package cover test/main
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- cmd/ipfs/runmain_test.go
+++ cmd/ipfs/runmain_test.go
@@ -5,13 +5,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
+// this abuses go so much that I felt dirty writing this code
+// but it is the only way to do it without writing custom compiler that would
+// be a clone of go-build with go-test
 func TestRunMain(t *testing.T) {
 	args := flag.Args()
 	os.Args = append([]string{os.Args[0]}, args...)
 	ret := mainRet()
-	fmt.Println(ret)
+	p := os.Getenv("IPFS_COVERT_RET_FILE")
+	if len(p) != 0 {
+		ioutil.WriteFile(p, []byte(fmt.Sprintf("%d\n", ret)), 0777)
+	}
+
+	// close outputs so go testing doesn't print anything
+	null, err := os.Open("/dev/null")
+	if err == nil {
+		os.Stderr = null
+		os.Stdout = null
+	}
 }
