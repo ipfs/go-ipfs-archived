98fc2c242f74eaa8455080c5eeb74102d68f4a49
Jakub Sztandera <kubuxu@protonmail.ch>
2016-12-16 00:06:07 +0100

    Now?? pls...
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- coverage/Rules.mk
+++ coverage/Rules.mk
@@ -38,8 +38,9 @@ endif
 
 export IPFS_COVER_DIR:= $(realpath $(d))/sharnesscover/
 
-$(d)/sharness_tests.coverprofile: $(d)/ipfs cmd/ipfs/ipfs-test-cover $(d)/coverage_deps test_sharness_short
-	(cd $(@D)/sharnesscover && bash -c 'gocovmerge *' ) > $@
+$(d)/sharness_tests.coverprofile: #$(d)/ipfs cmd/ipfs/ipfs-test-cover $(d)/coverage_deps test_sharness_short
+	$(eval TMP_ALL := $(shell (cd $(@D) && ls -1 .))
+	(cd $(@D)/sharnesscover && gocovmerge $(TMP_ALL)) > $@
 
 
 PATH := $(realpath $(d)):$(PATH)
