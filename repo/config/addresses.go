88d53fd9ee66eecde7dbc7a023ce1459670e9d1d
Lars Gierth <larsg@systemli.org>
2017-04-19 01:03:26 +0200

    WIP
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- repo/config/addresses.go
+++ repo/config/addresses.go
@@ -2,7 +2,9 @@ package config
 
 // Addresses stores the (string) multiaddr addresses for the node.
 type Addresses struct {
-	Swarm   []string // addresses for the swarm network
-	API     string   // address for the local API (RPC)
-	Gateway string   // address to listen on for IPFS HTTP object gateway
+	Swarm      []string // addresses the swarm should listen on
+	Announce   []string // addresses the swarm should announce to the network
+	NoAnnounce []string // addresses the swarm should not announce to the network
+	API        string   // address for the local API (RPC)
+	Gateway    string   // address to listen on for IPFS HTTP object gateway
 }
